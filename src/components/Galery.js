c712240d0edfb7f683b16aa3ac29165b9f1320a7
angelito <angelmartinezwb@gmail.com>
2021-01-27 06:09:56 -0300

    separeted styles.scss into styles, background and fonts for a better organization

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -1,55 +1,69 @@
 import React, {Fragment, Component} from 'react'
 
 export default class Galery extends Component{
+  //Initial states are defined into constructor
   constructor(props){
     super(props)
     this.state={
+      //empty array that will be the json list from the API call
       memes: [],
+      //a integer that sets how many items from the array will be rendered
       limit: 8,
+      //the button that load more items from the array, with the function call for these
       loadMore: (<button  className='load-more-btn'type='button' onClick={this.loadMore}>Load more...</button>)
     }
   }
+  //when the component has been rendered, this function will call to API
   componentDidMount(){
+    //API's link
     fetch('https://api.imgflip.com/get_memes')
+      //Promise, when the response is received, is converted to a .json
       .then((response)=>{
         return response.json()
       })
+      //Promise, the response (list) is seted into the "memes" state
       .then((list)=>{
         this.setState({
           memes: list.data.memes
         })
       })
   }
+  //Function that load more items from the array
   loadMore=()=>{
     if(this.state.limit < this.state.memes.length){
       this.setState({
+        //the limit state is increased by 8
         limit: this.state.limit + 8
       })
     }else{
       this.setState({
+        //if there is no more items to show, the "loadMore" state is changed to a paragraph
         loadMore: (<p className='load-more-btn'>This is all here!!</p>)
       })
     }
   }
   render(){
+    //If the response isn't set yet, retur a simple text box
     if(this.state.memes <= 0){
       return(
         <div id='loading'>Loading... Wait a ding dang diddly time</div>
       )
-    }else {
+    }else {//when the response is avaible, return the "galery" component
       return(
         <Fragment>
           <main id='galery'>
+            {/*an iterator to show the items from state array "memes"*/}
             {this.state.memes.slice(0, this.state.limit).map((memes)=>{
               return(
                 <article className='galery-item'key={memes.id}>
                   <h2>{memes.name}</h2>
                   <img src={memes.url} alt={memes.name}></img>
-                  <a download={memes.id+".jpg"} href={memes.url} target='_blank' >Download</a>
+                  <a download={memes.id+".jpg"} href={memes.url} target='_blank' rel='noreferrer'>Download</a>
                 </article>
               )
             })}
           </main>
+          {/*this is the button that load more items*/}
           {this.state.loadMore}
         </Fragment>
       )
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 
 export default class Random extends Component{
+  //Initial states are defined into constructor
   constructor(props){
     super(props)
     this.state={
+      //empty array that will be the json list from the API call
       memes: [],
+      //a integer that used to take the item's id from the array
       random: 0,
+      //a initial boolean that set if the item is rendered or not
       getRandom: false
     }
   }
+  //calling to the API
   componentDidMount(){
     fetch('https://api.imgflip.com/get_memes')
     .then((response)=>{
@@ -18,28 +23,32 @@ export default class Random extends Component{
       this.setState({memes: list.data.memes})
     })
   }
+  //getting a random integer
   getRandom=()=>{
     let random = Math.floor(Math.random() * (this.state.memes.length - 0)) + 0
+    //the boolean is seting to true and the random state is seted to random local variable
     this.setState({
       getRandom: true,
       random: random
     })
   }
   render(){
+    //if the getRandom function isn´t called yet, just show a simple button that calls it for first time
     if(this.state.getRandom === false){
       return(
         <main id='random'>
           <button onClick={this.getRandom}>Get your lucky meme!</button>
         </main>
       )
-    }else{
+    }else{//else if the function has been called (so getRAndom state is true now) return the item that it id matches with the "random" integer into states
       return(
         <main id='random'>
           <article className='random-item'key={this.state.memes[this.state.random].id}>
             <h2>{this.state.memes[this.state.random].name}</h2>
             <img src={this.state.memes[this.state.random].url} alt={this.state.memes[this.state.random].name}></img>
-            <a download={this.state.memes[this.state.random].id+".jpg"} href={this.state.memes[this.state.random].url} target='_blank' >Download</a>
+            <a download={this.state.memes[this.state.random].id+".jpg"} href={this.state.memes[this.state.random].url} target='_blank' rel='noreferrer'>Download</a>
           </article>
+          {/*Calling again to get a new item*/ }
           <button onClick={this.getRandom}>Don't want this?? Try again!</button>
         </main>
       )
